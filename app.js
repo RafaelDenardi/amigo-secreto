bba721974cafc5fb94efd237d60118a5210ecee3
Rafael Denardi <rdenardird@gmail.com>
2025-08-11 19:22:53 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 let listaAmigoSecreto = [];
 
 function adicionarAmigo(){
-    let amigos = document.querySelector('#amigo');
+    let amigos = document.getElementById('amigo');
     let nome = amigos.value;
-    let lista = document.getElementById ('listaAmigos');
+    
     if (nome === ''){
         alert ('Por favor, insira um nome.');
         return;
-    } else {
+    } else if (listaAmigoSecreto.some(amigo => amigo.toLowerCase() === nome.toLowerCase())) {
+    alert('Esse nome já foi adicionado.');
+    return;
+    }else {
         listaAmigoSecreto.push(nome);
     }
     amigos.value = '';
-    console.log(listaAmigoSecreto);
+    exibirListaAmigos();
 
+}
+
+function exibirListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = '';
+
+    for (let i = 0; i < listaAmigoSecreto.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = listaAmigoSecreto[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    let amigos = listaAmigoSecreto;
+
+    if (amigos.length === 0) {
+        alert('A lista de amigos está vazia. Adicione nomes antes de sortear.');
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultadoElemento = document.getElementById('resultado');
+    resultadoElemento.innerHTML = `<li>Amigo sorteado: <strong>${amigoSorteado}</strong></li>`;
 }
